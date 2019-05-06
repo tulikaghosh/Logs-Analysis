f3f7e3952ed530a46757d35100841e94d544fcea
tulikaghosh <40379664+tulikaghosh@users.noreply.github.com>
2019-05-06 15:19:48 -0400

    Create Article.py

---
+++ Article.py
@@ -0,0 +1,30 @@
+import psycopg2 as pg2
+import sys
+
+# Most popular article
+connect = pg2.connect(database='news')
+
+cursor = connect.cursor()
+ 
+cursor.execute('SELECT * FROM most_popular_articles LIMIT 3;')
+data = cursor.fetchall()
+for line in data:
+    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
+print ('\n')
+cursor.close()
+# Most popular author
+connect = pg2.connect(database='news')
+cursor = connect.cursor()
+cursor.execute('SELECT * FROM most_popular_author;')
+data = cursor.fetchall()
+for line in data:
+    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
+print ('\n')
+# Most error day
+connect = pg2.connect(database='news')
+cursor = connect.cursor()
+cursor.execute('SELECT * FROM errors_day;')
+data = cursor.fetchall()
+for line in data:
+    print(str(line[0]) + ' - ' + str(line[1]) +' errors')
+print ('\n')  
