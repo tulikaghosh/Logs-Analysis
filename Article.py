8d9e31335fc8bc9f0a68639bc4869d684bdec2df
tulikaghosh <40379664+tulikaghosh@users.noreply.github.com>
2019-05-07 23:52:11 -0400

    Update Article.py

--- Article.py
+++ Article.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import psycopg2 as pg2
 import sys
 
@@ -5,26 +7,31 @@ import sys
 connect = pg2.connect(database='news')
 
 cursor = connect.cursor()
- 
 cursor.execute('SELECT * FROM most_popular_articles LIMIT 3;')
 data = cursor.fetchall()
 for line in data:
-    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
-print ('\n')
+    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + 'views')
+print '\n'
 cursor.close()
+
+
 # Most popular author
 connect = pg2.connect(database='news')
 cursor = connect.cursor()
 cursor.execute('SELECT * FROM most_popular_author;')
 data = cursor.fetchall()
 for line in data:
-    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
-print ('\n')
+    print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + 'views')
+print '\n'
+cursor.close()
+
+
 # Most error day
 connect = pg2.connect(database='news')
 cursor = connect.cursor()
 cursor.execute('SELECT * FROM errors_day;')
 data = cursor.fetchall()
 for line in data:
-    print(str(line[0]) + ' - ' + str(line[1]) +' errors')
-print ('\n')  
+    print(str(line[0]) + ' - ' + str(line[1]) + 'errors')
+print("")
+cursor.close()
